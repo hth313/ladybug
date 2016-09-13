131aa236b6d086236d649e29d23f2958c6755d54
Håkan Thörngren <hth313@gmail.com>
2016-09-13 09:26:00 -0700

    Correct use in page 7

--- src/linker.scm
+++ src/linker.scm
@@ -2,7 +2,7 @@
   '((memory Programmer1 (position independent) (bank 1) (address (#x0 . #xFFF))
               ;; Comment: Need KeyTable at a specific location
             (section (FAT #x0) Code Code1 (CodeQ0 (#x0 . #x3FF))
-                     (Legal7 #xCCD)
+                     (Legal7 #xCDD)
                      (KeyTable #xC00) (Tail #xFD4))
             (checksum #xFFF hp41)
             (fill 0))
