587945ba8d0f15e1787a068138f23d13bb31eb77
Håkan Thörngren <hth313@gmail.com>
2017-01-13 12:32:45 -0800

    Make it work with early 41C ROMs
    The PACH11 fix is not available and should not be called. We do the
    same thing in the page 4 ROM instead so it works on these early models
    too.

--- src/osExtension.s
+++ src/osExtension.s
@@ -17,14 +17,15 @@
               c=stk                 ; inspect return address
               rcr     1             ; C.XS= lower nibble
               c=c+c   xs
-              goc     deepWake
+              golc    DSWKUP+2      ; deep wake up
 
-lightWake:    gosub   PACH11
+lightWake:    ldi     0x2fd         ; PACH11
+              dadd=c                ; enable nonexistent data chip 2FD
+              pfad=c                ; enable display
+              flldc                 ; non-destructive read
+              gosub   MEMCHK
               ldi     8             ; I/O service
               gosub   ROMCHK
               chk kb
               golc    0x1a6
               golong  0x18a
-
-
-deepWake:     golong  DSWKUP+2
