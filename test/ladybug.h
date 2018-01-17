b0286491d5a30bd9407bdf0ca97d21d9c601fd66
Håkan Thörngren <hth313@gmail.com>
2018-01-16 21:42:55 -0800

    Make use of preprocessor in RPN compiler
    Move all .postfix directives to a central header file.
    Use a macro to handle the #LIT instruction as the preprocessor gets
    confused by it. Fetch the Ladybug module exports using -I to the src/
    directory so that it is defined in one place only.

---
+++ test/ladybug.h
@@ -0,0 +1,15 @@
+#ifndef __LADYBUG_H
+#define __LADYBUG_H
+
+.postfix ALDI, PSEI, DSZI, DECI, INCI, WSIZE, CLRI (semi-merged-postfix)
+.postfix LDI, STI, CMP, SEX                        (semi-merged-postfix)
+.postfix SB, CB, B?                                (semi-merged-postfix)
+.postfix RR, RL, RRC, RLC, ASR, SL, SR             (semi-merged-postfix)
+.postfix MASKL, MASKR, BITSUM, TST                 (semi-merged-postfix)
+.postfix #LIT                                      (semi-merged-integer-literal)
+
+// Using an instruction with a # makes the C preprocessor complain, we hide
+// it using a macro.
+#define integer #LIT
+
+#endif // __LADYBUG_H
