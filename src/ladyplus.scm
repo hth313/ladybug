ff4df8de972e476994acc3ecc2d7ec2f612a25ca
Håkan Thörngren <hth313@gmail.com>
2020-04-01 22:19:42 -0700

    Add some more boost sections

--- src/ladyplus.scm
+++ src/ladyplus.scm
@@ -17,6 +17,9 @@
             (bank 1) (address (#x0 . #xFFF))
             (section (BoostFAT #x0) BoostCode BoostTable Lib41Code
                      BoostSecondary BoostSecondary1
+                     (ExtensionHandlers #xF00)
+                     (CAT7Shell #xF04)
+                     (CatShell #xF0C)
                      RPN (BoostFC2 #xFC2) (BoostPoll #xFE0))
             (checksum #xFFF hp41)
             (fill 0))
