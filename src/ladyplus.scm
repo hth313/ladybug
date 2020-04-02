7eee0afb16a1adf5f33aae9293faaec2fa73f1b4
Håkan Thörngren <hth313@gmail.com>
2020-04-01 22:38:22 -0700

    Reserve space for NoV-64 control words

--- src/ladyplus.scm
+++ src/ladyplus.scm
@@ -54,6 +54,7 @@
                      (fixedEntries #x4d00)
                      (keycode #x4a40)
                      (OS4BankSwitchers1 #x4fc3)
+                     (NOV64 #x4100)
                      (entry #x4f00)
                      (TailOS4 #x4ffb))
             (checksum #x4FFF hp41)
@@ -61,6 +62,7 @@
     (memory OS4-2 (bank 2) (address (#x4000 . #x4FFF))
             (section (Header4_2 #x4000)
                      code2
+                     (NOV64_B2 #x4100)
                      (OS4BankSwitchers2 #x4fc3)
                      (TailOS4_2 #x4ffb))
             (checksum #x4FFF hp41)
