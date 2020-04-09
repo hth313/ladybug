d67eacd3ce9485c4714b12a92885b05808adcc25
Håkan Thörngren <hth313@gmail.com>
2020-04-09 12:42:11 -0700

    Ladyplus now includes the banked Boost module

--- src/ladyplus.scm
+++ src/ladyplus.scm
@@ -13,17 +13,25 @@
             (checksum #xFFF hp41)
             (fill 0))
     ;; Boost
-    (memory Boost (position independent)
+    (memory Boost1 (position independent)
             (bank 1) (address (#x0 . #xFFF))
-            (section (BoostFAT #x0) BoostCode BoostTable Lib41Code
-                     BoostSecondary BoostSecondary1
+            (section (BoostFAT #x0) BoostCode BoostCode1 BoostTable Lib41Code
+                     BoostSecondary1
                      (ExtensionHandlers #xF00)
                      (CAT7Shell #xF04)
                      (CatShell #xF0C)
+                     (BoostBankSwitchers1 #xFC3)
                      RPN (BoostFC2 #xFC2) (BoostPoll #xFE0))
             (checksum #xFFF hp41)
             (fill 0))
-    ;; Mainframe
+    (memory Boost2 (position independent)
+            (bank 2) (address (#x0 . #xFFF))
+            (section (BoostHeader2 #x0) BoostCode2
+                     BoostSecondary2
+                     (BoostBankSwitchers2 #xFC3)
+                     (BoostTail2 #xFF4))
+            (checksum #xFFF hp41)
+            (fill 0))    ;; Mainframe
     (memory NUT0 (bank 1) (address (#x0 . #xFFF))
             (section (QUAD0 #x0) (QUAD1 #x400) (QUAD2 #x800) (QUAD3 #xC00))
             (checksum #xFFF hp41)
